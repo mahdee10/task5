2d3cb00e59dd4e33d8b06596529fc955429f6994
mahdee10 <diranimahdi2002@gmail.com>
2023-09-14 11:03:07 +0200

    aa

--- app/components/swiper.js
+++ app/components/swiper.js
@@ -1,13 +1,15 @@
 'use client'
-import React from 'react';
+import React, { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css'; // Import Swiper styles
 import Image from "next/image";
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Navigation, Pagination, Scrollbar, A11y,EffectCoverflow  } from 'swiper/modules';
 const MySwiperComponent = ({ jobs }) => {
+    const [activeSlideIndex, setActiveSlideIndex] = useState(0);
+  
     // Check if jobs is defined and not null
     if (!jobs || jobs.length === 0) {
       return null; // Or render a message indicating no jobs are available
@@ -15,24 +17,34 @@ const MySwiperComponent = ({ jobs }) => {
   
     return (
       <Swiper
-      modules={[Navigation]}
-      spaceBetween={50}
-      slidesPerView={3}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
+        modules={[Navigation,Pagination,  EffectCoverflow]} // Add EffectCoverflow module
+        spaceBetween={50}
+        slidesPerView={3}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        effect="coverflow" // Enable coverflow effect
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={(swiper) => {
+          console.log('slide change', swiper.activeIndex);
+          setActiveSlideIndex(swiper.activeIndex);
+        }}
       >
-        {jobs.map((job,index) => (
+        {jobs.map((job, index) => (
           <SwiperSlide key={job.title}>
-            <div className="image-container2 flex justify-center">
-              <Image className="my-0 mx-2.5 w-80 h-56 blur-sm"  src={job.path}  alt="" />
-
+            <div
+              className={` flex justify-center ${
+                index === activeSlideIndex+1 ? '' : ''
+              }`}
+            >
+              <Image className={`my-0 mx-2.5 w-80 h-56 blur-sm ${
+                index === activeSlideIndex+1 ? 'middle-slide' : ''
+              }`}  src={job.path} alt="" />
+  
               <div className="overlay absolute top-2/4 left-1/2 text-white p-5 text-center bg-dark-blur">
-              {/* <h2 className={`text-2xl font-bold ${index === 1 ? 'text-dark-pink' : ''}`}>{job.title}</h2> */}
-              <h2 className="text-2xl font-bold ">{job.title}</h2>
-
+                <h2 className={`text-2xl font-bold ${index === 1 ? '' : ''}`}>
+                  {job.title}
+                </h2>
                 <p>{job.experience}</p>
                 <p>{job.salary}</p>
               </div>
@@ -42,6 +54,7 @@ const MySwiperComponent = ({ jobs }) => {
       </Swiper>
     );
   };
+  
 
 export default MySwiperComponent;
 
