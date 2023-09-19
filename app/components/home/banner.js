ee3acf0b43760d313b2ccdb406bc140b141be4f4
mahdee10 <diranimahdi2002@gmail.com>
2023-09-19 11:32:59 +0200

    asc

--- app/components/home/banner.js
+++ app/components/home/banner.js
@@ -1,13 +1,31 @@
+// components/Banner.js
+
 import React from "react";
 import Image from "next/image";
-export default function Banner(props){
-    return(
-        <div className="my-1.5 mb-4 sm: relative image-container2 ">
-                <Image onClick={()=>props.fieldChange(props.title)} className=" my-0 mx-2.5 w-80 h-56 blur-sm "  src={props.path}  alt="" />
-                <div className="overlay absolute top-2/4 left-1/2 text-white p-5 text-center bg-dark-blur">
-                    <h2 className="text-2xl font-bold">{props.title}</h2>
-                    <p>{props.content}</p>
-                </div>
-            </div>
-    )
-}
+
+const Banner = (props) => {
+  const handleImageClick = () => {
+    props.fieldChange(props.title);
+  };
+
+  return (
+    <div className="my-1.5 mb-4 sm: relative image-container2">
+      <Image
+        onClick={handleImageClick}
+        className="my-0 mx-2.5 w-80 h-56 blur-sm"
+        src={props.path}
+        alt=""
+      />
+      <div className="overlay absolute top-2/4 left-1/2 text-white p-5 text-center bg-dark-blur">
+        <h2 className="text-2xl font-bold">{props.title}</h2>
+        <p>{props.content}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
+
+
+
+
