5cb025229fcf8bb42d8262842b4847f978fb513a
mahdee10 <diranimahdi2002@gmail.com>
2023-09-19 11:26:40 +0200

    mah

--- app/components/home/form.js
+++ app/components/home/form.js
@@ -2,6 +2,7 @@
 
 
 const MyForm = (props) => {
+  const field = props.choose || '';
 
   return (
     <form className="text-center m-5 text-black">
@@ -11,12 +12,14 @@ const MyForm = (props) => {
           type="text"
           name="name"
           placeholder="Name"
+          autoComplete="on"
         />
         <input
           className="w-full p-2.5 my-1.5 mx-0"
 
           type="email"
           name="email"
+          autoComplete="on"
           placeholder="Email"
         />
         <input
@@ -25,7 +28,8 @@ const MyForm = (props) => {
           type="text"
           name="Field"
           placeholder="Field (Click on the field you are interested in)"
-          value={props.choose}
+          value={field}
+          autoComplete="on"
           readOnly
         />
       <button className="w-full p-2.5 my-1.5 mx-0 bg-gray-800 border-0 cursor-pointer"  type="submit">Submit</button>
